a64dfb3ab1824713c2c535a7b5f58f7737fde25a
Choi9033 <134060811+Choi9033@users.noreply.github.com>
2024-12-08 00:27:39 +0900

    DBConnecting source
    로컬DB연동용

--- DBdelivery-main (1)/DBdelivery/src/DatabaseConnection.java
+++ DBdelivery-main (1)/DBdelivery/src/DatabaseConnection.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:oracle:thin:@211.109.73.83:1521:xe";
+    private static final String URL = "jdbc:oracle:thin:@localhost:xe";
     private static final String USERNAME = "c##BAEMIN"; // Oracle DB 사용자 이름
     private static final String PASSWORD = "1234"; // Oracle DB 비밀번호
 
